24debdb9dc077f9be3cb214cc83ec676ea1bcdbf
mariusrik <marius.rikheim@gmail.com>
2017-12-09 14:35:41 +0100

    added zuul security example from tsds repo ready for adaption. Might need some mvn dependencies

--- zuul/src/main/kotlin/GatewayApplication.kt
+++ zuul/src/main/kotlin/GatewayApplication.kt
@@ -2,11 +2,18 @@
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @EnableZuulProxy
 @SpringBootApplication
-class GatewayApplication {
+class GatewayApplication{
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 }
 
 
