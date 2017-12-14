6c9cbcb076526c4ce0f2f0f81768203a7513166f
odeand14 <aodegaar@gmail.com>
2017-12-14 15:29:33 +0100

    And local security config for testing..

---
+++ highscore/src/test/kotlin/no/westerdals/highscore/WebSecurityConfigLocalFake.kt
@@ -0,0 +1,30 @@
+package no.westerdals.highscore
+
+
+// Created by Andreas Ødegaard on 13.12.2017.
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+
+/*
+  When testing this service in isolation, we would not have Zuul up and running.
+  So we override the default security configuration to use an in-memory
+  set of users/passwords.
+ */
+@Configuration
+@EnableWebSecurity
+@Order(1)
+class WebSecurityConfigLocalFake : WebSecurityConfig() {
+
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+
+        auth.inMemoryAuthentication()
+                .withUser("foo").password("123").roles("USER").and()
+                .withUser("bar").password("123").roles("USER").and()
+                .withUser("admin").password("admin").roles("ADMIN", "USER")
+    }
+}
